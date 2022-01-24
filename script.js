c36df8d2eac6b8d3d65718c3759d16aa5ce73b51
Lari Oncescu <larioncescu@yahoo.com>
2022-01-24 17:21:33 +0200

    resolve a bug

--- script.js
+++ script.js
@@ -205,8 +205,8 @@ const now = new Date();
 const year = now.getFullYear();
 const month = `${now.getMonth() + 1}`.padStart(2, 0);
 const day = `${now.getDate()}`.padStart(2, 0);
-const hour = now.getHours();
-const minutes = now.getMinutes();
+const hour = `${now.getHours()}`.padStart(2, 0);
+const minutes = `${now.getMinutes()}`.padStart(2, 0);
 labelDate.textContent = `${day}/${month}/${year}, ${hour}:${minutes}`;
 
 btnLogin.addEventListener('click', function (e) {
